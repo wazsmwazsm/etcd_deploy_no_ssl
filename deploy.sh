139b26ede42c7b8c0e1675dd6671c452e2ab3f9b
qinjiaqi <qinjiaqi@mafengwo.com>
2019-10-28 19:12:33 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@
 echo "=====> copy config file..."
 mkdir -p /usr/local/etcd/bin
 mkdir -p /usr/local/etcd/conf
+chown -R daemon:daemon /usr/local/etcd/
 cp etcd.conf /usr/local/etcd/conf
 cp etcd-v3.4.3-linux-amd64/etcd /usr/local/etcd/bin
 cp etcd-v3.4.3-linux-amd64/etcdctl /usr/local/etcd/bin
